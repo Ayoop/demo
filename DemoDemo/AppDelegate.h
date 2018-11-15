196aec5cb37a4f27e063e9dafc361ae765dcdb05
Ayoop <2271475399@qq.com>
2018-11-15 11:49:46 +0800

    tttt

---
+++ DemoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoDemo
+//
+//  Created by 范礼聪 on 2018/11/15.
+//  Copyright © 2018年 范礼聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoDemo
+//
+//  Created by 范礼聪 on 2018/11/15.
+//  Copyright © 2018年 范礼聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
